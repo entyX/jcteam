39266540fefa494421301e7829f4daa840161382
entyX <119726554+entyX@users.noreply.github.com>
2023-09-24 17:01:00 -0700

    Update entyland.js

--- games/Entyland/entyland.js
+++ games/Entyland/entyland.js
@@ -29,25 +29,19 @@ showVar();
 
         function buyLand() {
             if (localStorage.land) {
-              if (localStorage.gold > 0) {
+              if (localStorage.gold > 50) {
                 localStorage.land = Number(localStorage.land)+1;
                 localStorage.gold = Number(localStorage.gold)-50;
-                if (localStorage.gold < 0){
-                  localStorage.gold = 0;
-                  document.getElementById("news").innerText = "It's not a good idea to go in debt!";
-                } else {
                 document.getElementById("news").innerText = "You conquered land and spent 50 gold on the border!";
                 }
-              } else {
-                alert("Not enough gold...")
+                else {
                 document.getElementById("news").innerText = "You realized you didn't have enough gold...";
-                
               }
             } else {
               localStorage.land = 1;
             }
             document.getElementById("land").innerText = "Land (acres): "+localStorage.land;
-            document.getElementById("gold").innerText = "Gold: "+localStorage.gold; 
+            document.getElementById("gold").innerText = "Gold: "+Math.floor(localStorage.gold); 
         }
 
         function rndInt(min, max) {
@@ -82,11 +76,9 @@ showVar();
                         }
 
                     } else {
-                        alert("Not enough tools...")
                         document.getElementById("news").innerText = "You realized that you didn't have enough tools...";
                     }
                 } else {
-                    alert("Not enough land...")
                     document.getElementById("news").innerText = "You realized that there was no more land to clear...";
                 }
             } else {
@@ -114,11 +106,9 @@ showVar();
               document.getElementById("news").innerText = "You just crafted 5 toolkits for your settlement!";
 
             } else {
-              alert("Not enough wood...")
               document.getElementById("news").innerText = "You sadly didn't have enough wood to craft toolkits...";
             }
           } else {
-            alert("Not enough stone...")
             document.getElementById("news").innerText = "You sadly didn't have enough stone to craft toolkits...";
           }
         }
@@ -138,14 +128,12 @@ showVar();
               document.getElementById("news").innerText = "You just built a house in your settlement!";
 
               localStorage.people = Number(localStorage.people)+4;
-              document.getElementById("people").innerText = "People: "+localStorage.people;
+              document.getElementById("people").innerText = "People: "+Math.floor(localStorage.people);
 
             } else {
-              alert("Not enough wood...")
               document.getElementById("news").innerText = "You sadly didn't have enough wood to build a house...";
             }
           } else {
-            alert("Not enough stone...")
             document.getElementById("news").innerText = "You sadly didn't have enough stone to build a house...";
           }
         }
@@ -161,15 +149,14 @@ showVar();
               document.getElementById("food").innerText = "Food: "+localStorage.food;
               document.getElementById("wood").innerText = "Wood: "+localStorage.wood;
               document.getElementById("stone").innerText = "Stone: "+localStorage.stone;
+              document.getElementById("farm").innerText = "Farms: "+localStorage.farm;
               document.getElementById("cland").innerText = "Cleared Land (acres): "+localStorage.cland;
               document.getElementById("news").innerText = "You just built a farm in your settlement!";
 
             } else {
-              alert("Not enough wood...")
               document.getElementById("news").innerText = "You sadly didn't have enough wood to build a farm...";
             }
           } else {
-            alert("Not enough stone...")
             document.getElementById("news").innerText = "You sadly didn't have enough stone to build a farm...";
           }
         }
@@ -199,7 +186,7 @@ showVar();
           if (Ftimer == 0) {
             clearInterval(intF);
               Ftimer = 3;
-              localStorage.food = Number(localStorage.food)+(1 * localStorage.farm);
+              localStorage.food = Number(localStorage.food)+(localStorage.farm * localStorage.people);
               document.getElementById("food").innerText = "Food: "+Math.floor(localStorage.food);
               intF = setInterval(farmInterval, 1000);
           }
